e06acdf468b5eb8c7062d4409d3dfd4bdc9ac61d
cgw87 <cgw87@HunJe>
2023-04-26 20:26:28 +0900

    2023/04/26 최훈제 패키지 생성

--- src/main/java/com/example/controller/PageTest.java
+++ src/main/java/com/example/controller/PageTest.java
@@ -3,12 +3,15 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class PageTest {
 	
 	@GetMapping("test")
 	public String getPage() {
-		
+		log.info("test 페이지 시작");
 		return "test";
 	}
 }
