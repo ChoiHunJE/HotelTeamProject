b02488e02936e9f9c8b8a335bdbe146767fd10dc
82104 <82104@LAPTOP-H7ALL1A8>
2023-04-29 15:31:39 +0900

    2023.04.29김세민 reservation 페이지 작동 완료

--- src/main/java/com/example/controller/ReservationController.java
+++ src/main/java/com/example/controller/ReservationController.java
@@ -2,12 +2,18 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class ReservationController {
+	
+	//예약하기 페이지 이동
 	@GetMapping("reservation")
-	public String ReservationForm() {
-		return "";
+	public String Reservation() {
+		log.info("reservation 실행");
+		return "reservation/reservation";
 	}
 }
