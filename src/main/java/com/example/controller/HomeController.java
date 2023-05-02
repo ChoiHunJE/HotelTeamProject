0dcff5d26eef62b5cce872fce974ab74a9641f2e
cgw87 <cgw87@HunJe>
2023-05-02 11:12:41 +0900

    2023/05/02 최훈제 HomeController

---
+++ src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
