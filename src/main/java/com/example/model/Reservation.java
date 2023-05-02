530e0c6d4495e8aac7465bc546bc0e5182a88224
82104 <82104@LAPTOP-H7ALL1A8>
2023-05-02 11:21:18 +0900

    2023.05.02 김세민 db베이스 생성

--- src/main/java/com/example/model/Reservation.java
+++ src/main/java/com/example/model/Reservation.java
@@ -1,5 +1,14 @@
 package com.example.model;
 
-public class Reservation {
+import java.time.LocalDateTime;
 
+import lombok.Data;
+
+@Data
+public class Reservation {
+	private Long res_id;
+	private Long cust_id;
+	private LocalDateTime check_in;
+	private LocalDateTime check_out;
+	private Long capacity;
 }
--- src/main/java/com/example/model/Room.java
+++ src/main/java/com/example/model/Room.java
@@ -1,5 +1,11 @@
 package com.example.model;
 
-public class Room {
+import lombok.Data;
 
+@Data
+public class Room {
+	private Long room_id;
+	private String roomname;
+	private Long price;
+	private String info;
 }
