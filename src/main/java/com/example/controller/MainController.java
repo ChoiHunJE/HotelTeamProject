0db3c33df03eacd162affb60b252dd181f0e85af
cgw87 <cgw87@HunJe>
2023-05-03 08:20:23 +0900

    2023/05/03	최훈제

---
+++ src/main/java/com/example/controller/MainController.java
@@ -0,0 +1,29 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.model.Mail;
+import com.example.service.EmailService;
+
+@Controller
+public class MainController {
+	private final EmailService emailService;
+	 
+    public MainController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+ 
+    @GetMapping("/mail/send")
+    public String main() {
+        return "SendMail.html";
+    }
+ 
+    @PostMapping("/mail/send")
+    public String sendMail(Mail mail) {
+        emailService.sendSimpleMessage(mail);
+        System.out.println("메일 전송 완료");
+        return "redirect:/";
+    }
+}
