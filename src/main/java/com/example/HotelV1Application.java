04a73d02439f5fd77851425543df5c06e32d7878
82103 <82103@192.168.25.9>
2023-05-08 10:31:18 +0900

    Merge branch 'master' of https://github.com/ChoiHunJE/HotelTeamProject.git

--- src/main/java/com/example/HotelV1Application.java
+++ src/main/java/com/example/HotelV1Application.java
@@ -1,7 +1,12 @@
 package com.example;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class HotelV1Application {
@@ -9,5 +14,5 @@ public class HotelV1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(HotelV1Application.class, args);
 	}
-
+	
 }
--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -2,6 +2,11 @@ package com.example.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,7 +15,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(HttpServletRequest request) {
+    	
+    	Locale currentLocale = request.getLocale();
+    	String countryCode = currentLocale.getCountry();
+    	String countryName = currentLocale.getDisplayCountry();
+    	
+    	String langCode = currentLocale.getLanguage();
+    	String langName = currentLocale.getDisplayLanguage();
+    	
+    	System.out.println(countryCode + ": " + countryName);
+    	System.out.println(langCode + ": " + langName);
+    	
+    	System.out.println("=================");
+    	String[] languages = Locale.getISOLanguages();
+    	
+    	for (String language : languages) {
+    		Locale locale = new Locale(language);
+    		System.out.println(language + ": " + locale.getDisplayLanguage());
+    	}
+    	
         return "index";
     }
 }
